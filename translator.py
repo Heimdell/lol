12ef238e347c362498b08b5e7ae48b273375471e
kir <hindmost.one@gmail.com>
2015-11-21 14:58:03 +0400

    made name sanitizer

---
+++ translator.py
@@ -0,0 +1,32 @@
+
+from functools import *
+from operator import *
+
+known_ops = {
+    "+": "plus",
+    "@": "doge",
+    "$": "swag",
+    "-": "minus",
+    "*": "milt",
+    "/": "div",
+    "|": "stick",
+    "?": "wat",
+    ">": "more",
+    "<": "less"
+}
+
+def convert_char(char):
+    if char in known_ops:
+        return "_" + known_ops[char]
+    
+    if char >= 'A' and char <= 'z':
+        return char
+
+    return "_" + str(ord(char))
+
+safe = lambda op: lambda x, y: op(x, y) if x else y
+
+def convert_name(name):
+    return reduce(safe(add), map(convert_char, name))
+
+print(convert_name("<STDIN?>..."))
