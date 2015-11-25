20f4ebc09db89162d78c024d06e70d7049bc4a0f
kir <hindmost.one@gmail.com>
2015-11-25 12:37:30 +0400

    made compiler a runnable thing, accepting filenames to compile

--- compile.py
+++ compile.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3.5
 
 from tokenizer  import Tokenizer
 from syntax     import whole_program
 from translator import convert
+
+import sys
+import getopt
 
 def compile(filename):
     tokens = Tokenizer().file(filename)
@@ -12,4 +16,23 @@ def compile(filename):
     else:
         print(parse)
 
-compile("test.lol")
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+    except getopt.error(msg):
+        print(msg)
+        print("use with --help")
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print(__doc__)
+            sys.exit(0)
+
+    for arg in args:
+        sys.stdout.write("Compiling " + arg + "... ")
+        compile(arg)
+        print("done.")
+
+if __name__ == "__main__":
+    main()
