e608829a6c13d65e024462716d3b640404992f77
kir <hindmost.one@gmail.com>
2015-12-12 00:08:18 +0400

    Type inference kinda works

---
+++ src/hs/ASTNI2.hs
@@ -0,0 +1,236 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances    #-}
+
+import Control.Arrow
+import Control.Monad.State
+import Control.Monad.Reader
+import Control.Monad.Except
+
+import qualified Data.Set as Set
+import           Data.Set   (Set, member, singleton, empty, (\\))
+
+import qualified Data.Map as Map
+import           Data.Map   (Map)
+
+import Data.Monoid ((<>))
+
+data AST
+    = Let [Binding] AST
+    | App Term [Term]
+    deriving (Show, Eq)
+
+data Term
+    = Var   Name
+    | Const String
+    deriving (Show, Eq)
+
+data Binding
+    = Binding Name [Name] QType AST
+    deriving (Show, Eq)
+
+type Name = String
+
+data Type
+    = Hole Name
+    | Ground Name
+    | Type `To` Type
+    deriving (Show, Eq)
+
+data QType
+    = Forall (Set Name) Type
+    deriving (Show, Eq)
+
+newtype Substitution = Substitution { fromSubstitution :: [(Name, Type)] }
+    deriving (Show, Eq)
+
+(=:) :: Name -> Type -> Substitution
+(=:) = curry (Substitution . (:[]))
+
+(=:=) :: Name -> QType -> Environment
+(=:=) = curry (Map.fromList . (:[]))
+
+type Environment = Map Name QType
+
+instance Monoid Substitution where
+    mempty = Substitution mempty
+    mappend (Substitution l) (Substitution r) =
+        Substitution (l <> r)
+
+class WithTypes wt where
+    freeTypeVars :: wt -> Set Name
+    applyOne     :: (Name, Type) -> wt -> wt
+
+apply :: WithTypes wt => Substitution -> wt -> wt
+apply (Substitution list) x = foldl (flip applyOne) x list
+
+instance WithTypes Substitution where
+    freeTypeVars = mconcat . map (freeTypeVars . snd) . fromSubstitution
+    applyOne s      = Substitution . (s :) . fromSubstitution
+
+instance WithTypes Type where
+    freeTypeVars (Hole n)   = singleton n
+    freeTypeVars (d `To` i) = freeTypeVars d <> freeTypeVars i
+    freeTypeVars (Ground _) = empty
+
+    (n, t) `applyOne` Hole m | n == m = t
+    s      `applyOne` (dom `To` img)  = applyOne s dom `To` applyOne s img
+    _      `applyOne` other           = other
+
+instance WithTypes AST where
+    freeTypeVars (Let bs context) = freeTypeVars bs <> freeTypeVars context
+
+    s `applyOne` Let bs context = Let (applyOne s bs) (applyOne s context)
+    _ `applyOne` App f  xs      = App f xs
+
+instance WithTypes a => WithTypes [a] where
+    freeTypeVars = mconcat . map freeTypeVars
+    applyOne     = map . applyOne
+
+instance WithTypes Binding where
+    freeTypeVars (Binding _ _ ty _)        = freeTypeVars ty
+    s `applyOne` Binding name args ty body = Binding name args (applyOne s ty) body
+
+instance WithTypes QType where
+    freeTypeVars (Forall names ty) = freeTypeVars ty \\ names
+
+    s@(n, _) `applyOne` qt@(Forall names ty)
+        | n `elem` names = qt
+        | otherwise      = Forall names (applyOne s ty)
+
+instance WithTypes Environment where
+    freeTypeVars = mconcat . map freeTypeVars . Map.elems
+    applyOne     = Map.map . applyOne
+
+generalize :: Type -> TypeInference QType
+generalize t = do
+    env <- ask
+    return (Forall (free env) t)
+  where
+    free env = freeTypeVars t \\ freeTypeVars env
+
+type TypeInference = ExceptT String (ReaderT Environment (State Int))
+
+runTypeInference :: TypeInference a -> Either String a
+runTypeInference = (`evalState` 0) . (`runReaderT` Map.empty) . runExceptT
+
+runTypeInferenceKnowing knowledge 
+    = (`evalState` 0)
+    . (`runReaderT` knowledge)
+    . runExceptT
+
+fresh :: TypeInference Type
+fresh = do
+    number <- get
+    modify (+1)
+    return $ Hole (show number <> "?")
+
+instantiate :: QType -> TypeInference Type
+instantiate (Forall names ty) = do
+    let names' = Set.toList names
+    list <- forM names' $ \name -> do
+        ty <- fresh
+        return (name, ty)
+
+    return (apply (Substitution list) ty)
+
+unify :: Type -> Type -> TypeInference Substitution
+unify (a `To` b) (c `To` d) = do
+    s1 <- unify a c
+    s2 <- unify (apply s1 b) (apply s1 d)
+    return (apply s1 s2)
+
+unify (Hole a) b = bindVar a b
+unify a (Hole b) = bindVar b a
+unify (Ground _) (Ground _) = return mempty
+unify x y = throwError $ show x <> "~/~" <> show y
+
+bindVar :: Name -> Type -> TypeInference Substitution
+bindVar name ty
+    | ty == Hole name = return mempty
+    | name `member` freeTypeVars ty 
+        = throwError $ "cycle in type: " <> name <> " = " <> show ty
+    | otherwise
+        = return (Substitution [(name, ty)])
+
+class Inferrable i where
+    infer :: i -> TypeInference (Substitution, Type, i)
+
+instance Inferrable Term where
+    infer (Var name) = do
+        t <- lookupAtEnv name
+        return (mempty, t, Var name)
+
+    infer (Const x) = do
+        return (mempty, Ground "*", Const x)
+
+lookupAtEnv :: Name -> TypeInference Type
+lookupAtEnv name = do
+    env <- ask
+    t <- maybe (die ["what is ", name, "?"])
+     return (Map.lookup name env)
+
+    instantiate t
+
+die :: [String] -> TypeInference a
+die = throwError . concat
+
+instance Inferrable AST where
+    infer (App f xs) = do
+        tresult <- fresh
+        pile    <- mapM infer (f : xs)
+
+        let sxs        = substs pile
+        let (tf : txs) = types  pile
+
+        let tfi = foldl (flip To) tresult txs
+
+        su <- unify tf tfi
+
+        let alls = mconcat sxs <> su
+
+        return (alls, apply alls tresult, App f xs)
+
+    infer (Let bindings context) = do
+        pile <- forM bindings infer
+
+        let delta = decls pile
+        let ss    = mconcat (substs pile)
+
+        (sb, tb, b) <- local (delta <>) $ do
+            infer context
+
+        let alls = ss <> sb
+
+        return (alls, apply alls tb, apply alls (Let (results pile) b))
+
+substs =           map $ \(subst, _,  _) -> subst
+types  =           map $ \(_,     ty, _) -> ty
+decls  = mconcat . map ( \(_,     _,  Binding n _ qty _) -> n =:= qty )
+results =          map ( \(_,     _,  x) -> x )
+
+instance Inferrable Binding where
+    infer binding@(Binding name args ty body) = do
+        tresult          <- fresh
+        (func, bindings) <- freshTypesForArgs args tresult
+
+        ty'              <- instantiate ty
+        su               <- unify ty' func
+        
+        (sb, tb, b1) <- 
+            ((name =:= ty <> bindings) <>) `local` do
+                infer body
+
+        sbu <- unify tb tresult
+
+        let alls = su <> sb <> sbu
+        let tyn  = apply alls ty'
+        
+        ty1 <- generalize tyn
+
+        return (alls, tyn, Binding name args ty1 (apply alls body))
+      where
+        freshTypesForArgs []       result = return (result, mempty)
+        freshTypesForArgs (x : xs) result = do
+            (func, bs) <- freshTypesForArgs xs result
+            targ       <- fresh
+            return (targ `To` func, x =:= Forall mempty targ <> bs)
